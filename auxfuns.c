8fce2fd695894c2f181a0d911218d9a85f210e87
Werner <wstoop@gmail.com>
2015-02-23 11:08:15 +0200

    More coding standars changes

--- auxfuns.c
+++ auxfuns.c
@@ -1,8 +1,8 @@
 /*
- * Auxillary functions go here. 
+ * Auxillary functions go here.
  * These functions don't need intimate knowledge of the interpreter
  * for their implementation.
- * 
+ *
  * This is free and unencumbered software released into the public domain.
  * http://unlicense.org/
  */
@@ -13,215 +13,186 @@
 
 #include "fiz.h"
 
-char *fiz_readfile(const char *filename)
-{
-	FILE *f;
-	long len,r;
-	char *str;
-	
-	if(!(f = fopen(filename, "rb")))	
-		return NULL;
-	
-	fseek(f, 0, SEEK_END);
-	len = ftell(f);
-	rewind(f);
-	
-	if(!(str = malloc(len+2)))
-		return NULL;	
-	r = fread(str, 1, len, f);
-	
-	if(r != len) {
-		free(str);
-		return NULL;
-	}
-	
-	fclose(f);	
-	str[len] = '\0';
-	return str;
+char *fiz_readfile(const char *filename) {
+    FILE *f;
+    long len,r;
+    char *str;
+
+    if(!(f = fopen(filename, "rb")))
+        return NULL;
+
+    fseek(f, 0, SEEK_END);
+    len = ftell(f);
+    rewind(f);
+
+    if(!(str = malloc(len+2)))
+        return NULL;
+    r = fread(str, 1, len, f);
+
+    if(r != len) {
+        free(str);
+        return NULL;
+    }
+
+    fclose(f);
+    str[len] = '\0';
+    return str;
 }
 
-static enum Fiz_Code aux_puts(struct Fiz *F, int argc, char **argv, void *data)
-{
-	if(argc != 2) 
-		return fiz_argc_error(F, argv[0], 2);
-	puts(argv[1]);	
-	fiz_set_return(F, argv[1]);
-	return FIZ_OK;
+static enum Fiz_Code aux_puts(struct Fiz  *F, int argc, char **argv, void *data) {
+    if(argc != 2)
+        return fiz_argc_error(F, argv[0], 2);
+    puts(argv[1]);
+    fiz_set_return(F, argv[1]);
+    return FIZ_OK;
 }
 
-static enum Fiz_Code aux_expr(struct Fiz *F, int argc, char **argv, void *data)
-{
-	char *e;
-	const char *err;
-	int i;
-	size_t len = 0;
-	if(argc < 2) 
-		return fiz_argc_error(F, argv[0], 2);
-	for(i = 1; i < argc; i++)
-		len += strlen(argv[i]);
-	e = malloc(len+1);
-	e[0] = '\0';
-	for(i = 1; i < argc; i++)
-		strcat(e, argv[i]);	
-	assert(strlen(e) == len);	
-	i = expr(e, &err);
-	free(e);
-	if(err)
-	{
-		fiz_set_return_ex(F, "expr: %s", err);
-		return FIZ_ERROR;
-	}
-	fiz_set_return_ex(F, "%d", i);
-	return FIZ_OK;
+static enum Fiz_Code aux_expr(struct Fiz *F, int argc, char **argv, void *data) {
+    char *e;
+    const char *err;
+    int i;
+    size_t len = 0;
+    if(argc < 2)
+        return fiz_argc_error(F, argv[0], 2);
+    for(i = 1; i < argc; i++)
+        len += strlen(argv[i]);
+    e = malloc(len+1);
+    e[0] = '\0';
+    for(i = 1; i < argc; i++)
+        strcat(e, argv[i]);
+    assert(strlen(e) == len);
+    i = expr(e, &err);
+    free(e);
+    if(err) {
+        fiz_set_return_ex(F, "expr: %s", err);
+        return FIZ_ERROR;
+    }
+    fiz_set_return_ex(F, "%d", i);
+    return FIZ_OK;
 }
 
-static enum Fiz_Code aux_eqne(struct Fiz *F, int argc, char **argv, void *data)
-{
-	if(argc != 3) 
-		return fiz_argc_error(F, argv[0], 3);
-	if(!strcmp(argv[0], "eq"))
-		fiz_set_return_ex(F, "%d", !strcmp(argv[1],argv[2]));
-	else
-		fiz_set_return_ex(F, "%d", !!strcmp(argv[1],argv[2]));
-	return FIZ_OK;
+static enum Fiz_Code aux_eqne(struct Fiz *F, int argc, char **argv, void *data) {
+    if(argc != 3)
+        return fiz_argc_error(F, argv[0], 3);
+    if(!strcmp(argv[0], "eq"))
+        fiz_set_return_ex(F, "%d", !strcmp(argv[1],argv[2]));
+    else
+        fiz_set_return_ex(F, "%d", !!strcmp(argv[1],argv[2]));
+    return FIZ_OK;
 }
 
-static enum Fiz_Code aux_incr(struct Fiz *F, int argc, char **argv, void *data)
-{
-	const char *val;
-	int i;
-	if(argc != 2) 
-		return fiz_argc_error(F, argv[0], 2);
-	val = fiz_get_var(F, argv[1]);
-	if(!val)
-	{
-		fiz_set_return_ex(F, "%s not found", argv[1]);
-		return FIZ_ERROR;
-	}
-	i = atoi(val);
-	if(!strcmp(argv[0], "decr"))
-		i--;
-	else
-		i++;
-	fiz_set_var_ex(F, argv[1], "%d", i);
-	fiz_set_return_ex(F, "%d", i);	
-	return FIZ_OK;
+static enum Fiz_Code aux_incr(struct Fiz *F, int argc, char **argv, void *data) {
+    const char *val;
+    int i;
+    if(argc != 2)
+        return fiz_argc_error(F, argv[0], 2);
+    val = fiz_get_var(F, argv[1]);
+    if(!val) {
+        fiz_set_return_ex(F, "%s not found", argv[1]);
+        return FIZ_ERROR;
+    }
+    i = atoi(val);
+    if(!strcmp(argv[0], "decr"))
+        i--;
+    else
+        i++;
+    fiz_set_var_ex(F, argv[1], "%d", i);
+    fiz_set_return_ex(F, "%d", i);
+    return FIZ_OK;
 }
 
-static enum Fiz_Code aux_dict(struct Fiz *F, int argc, char **argv, void *data)
-{
-	const char *v = NULL;
-	if(argc < 3) 
-		return fiz_argc_error(F, argv[0], 3);	
-	if(!strcmp(argv[2], "put"))
-	{
-		if(argc < 5) 
-			return fiz_argc_error(F, argv[0], 5);		
-		fiz_dict_insert(F, argv[1], argv[3], argv[4]);
-		fiz_set_return(F, argv[4]);
-	}
-	else if(!strcmp(argv[2], "get"))
-	{
-		if(argc < 4) 
-			return fiz_argc_error(F, argv[0], 4);	
-		v = fiz_dict_find(F, argv[1], argv[3]);
-		if(!v)
-		{
-			fiz_set_return_ex(F, "no key %s in dict %s", argv[3], argv[1]);
-			return FIZ_ERROR;
-		}
-		fiz_set_return(F, v);
-	}
-	else if(!strcmp(argv[2], "has"))
-	{
-		if(argc < 4) 
-			return fiz_argc_error(F, argv[0], 4);	
-		v = fiz_dict_find(F, argv[1], argv[3]);
-		if(!v)
-			fiz_set_return(F, "0");
-		else
-			fiz_set_return(F, "1");
-	}
-	else if(!strcmp(argv[2], "first"))
-	{
-		if(argc < 3) 
-			return fiz_argc_error(F, argv[0], 3);		
-		v = fiz_dict_next(F, argv[1], NULL);
-		if(!v)
-		{
-			fiz_set_return_ex(F, "dict %s is empty or does not exist", argv[1]);
-			return FIZ_ERROR;
-		}
-		fiz_set_return(F, v);
-	}
-	else if(!strcmp(argv[2], "next"))
-	{
-		if(argc < 4) 
-			return fiz_argc_error(F, argv[0], 4);	
-		v = fiz_dict_next(F, argv[1], argv[3]);
-		if(!v)
-			fiz_set_return(F, "");
-		else 
-			fiz_set_return(F, v);
-	}
-	else if(!strcmp(argv[2], "remove"))
-	{
-		if(argc < 4) 
-			return fiz_argc_error(F, argv[0], 4);	
-		fiz_dict_delete(F, argv[1], argv[3]);
-		fiz_set_return(F, "");
-	}
-	else if(!strcmp(argv[2], "foreach"))
-	{
-		if(argc < 7) 
-			return fiz_argc_error(F, argv[0], 7);	
-		if(strcmp(argv[5], "do"))
-		{
-			fiz_set_return_ex(F, "syntax is: %s %s %s key val do {body}", argv[0], argv[1], argv[2]);
-			return FIZ_ERROR;
-		}							
-		while((v = fiz_dict_next(F, argv[1], v)) != NULL)
-		{
-			fiz_set_var(F, argv[3], v);
-			fiz_set_var(F, argv[4], fiz_dict_find(F, argv[1], v));
-			if(fiz_exec(F, argv[6]) != FIZ_OK)
-				return FIZ_ERROR;
-		}
-	}
-	else
-	{
-		fiz_set_return_ex(F, "unknown command %s to %s", argv[2], argv[0]);
-		return FIZ_ERROR;
-	}
-		
-	return FIZ_OK;
+static enum Fiz_Code aux_dict(struct Fiz *F, int argc, char **argv, void *data) {
+    const char *v = NULL;
+    if(argc < 3)
+        return fiz_argc_error(F, argv[0], 3);
+    if(!strcmp(argv[2], "put")) {
+        if(argc < 5)
+            return fiz_argc_error(F, argv[0], 5);
+        fiz_dict_insert(F, argv[1], argv[3], argv[4]);
+        fiz_set_return(F, argv[4]);
+    } else if(!strcmp(argv[2], "get")) {
+        if(argc < 4)
+            return fiz_argc_error(F, argv[0], 4);
+        v = fiz_dict_find(F, argv[1], argv[3]);
+        if(!v) {
+            fiz_set_return_ex(F, "no key %s in dict %s", argv[3], argv[1]);
+            return FIZ_ERROR;
+        }
+        fiz_set_return(F, v);
+    } else if(!strcmp(argv[2], "has")) {
+        if(argc < 4)
+            return fiz_argc_error(F, argv[0], 4);
+        v = fiz_dict_find(F, argv[1], argv[3]);
+        if(!v)
+            fiz_set_return(F, "0");
+        else
+            fiz_set_return(F, "1");
+    } else if(!strcmp(argv[2], "first")) {
+        if(argc < 3)
+            return fiz_argc_error(F, argv[0], 3);
+        v = fiz_dict_next(F, argv[1], NULL);
+        if(!v) {
+            fiz_set_return_ex(F, "dict %s is empty or does not exist", argv[1]);
+            return FIZ_ERROR;
+        }
+        fiz_set_return(F, v);
+    } else if(!strcmp(argv[2], "next")) {
+        if(argc < 4)
+            return fiz_argc_error(F, argv[0], 4);
+        v = fiz_dict_next(F, argv[1], argv[3]);
+        if(!v)
+            fiz_set_return(F, "");
+        else
+            fiz_set_return(F, v);
+    } else if(!strcmp(argv[2], "remove")) {
+        if(argc < 4)
+            return fiz_argc_error(F, argv[0], 4);
+        fiz_dict_delete(F, argv[1], argv[3]);
+        fiz_set_return(F, "");
+    } else if(!strcmp(argv[2], "foreach")) {
+        if(argc < 7)
+            return fiz_argc_error(F, argv[0], 7);
+        if(strcmp(argv[5], "do")) {
+            fiz_set_return_ex(F, "syntax is: %s %s %s key val do {body}", argv[0], argv[1], argv[2]);
+            return FIZ_ERROR;
+        }
+        while((v = fiz_dict_next(F, argv[1], v)) != NULL) {
+            fiz_set_var(F, argv[3], v);
+            fiz_set_var(F, argv[4], fiz_dict_find(F, argv[1], v));
+            if(fiz_exec(F, argv[6]) != FIZ_OK)
+                return FIZ_ERROR;
+        }
+    } else {
+        fiz_set_return_ex(F, "unknown command %s to %s", argv[2], argv[0]);
+        return FIZ_ERROR;
+    }
+
+    return FIZ_OK;
 }
 
 static enum Fiz_Code aux_include(struct Fiz *F, int argc, char **argv, void *data)
 {
-	enum Fiz_Code rc;
-	char *str;
-	if(argc != 2) 
-		return fiz_argc_error(F, argv[0], 2);
-	str = fiz_readfile(argv[1]);
-	if(!str)
-	{
-		fiz_set_return_ex(F, "unable to read %s", argv[1]);
-		return FIZ_ERROR;
-	}	
-	rc = fiz_exec(F, str);
-	free(str);
-	return rc;
+    enum Fiz_Code rc;
+    char *str;
+    if(argc != 2)
+        return fiz_argc_error(F, argv[0], 2);
+    str = fiz_readfile(argv[1]);
+    if(!str) {
+        fiz_set_return_ex(F, "unable to read %s", argv[1]);
+        return FIZ_ERROR;
+    }
+    rc = fiz_exec(F, str);
+    free(str);
+    return rc;
 }
 
-void fiz_add_aux(struct Fiz *F)
-{
-	fiz_add_func(F, "puts", aux_puts, NULL);
-	fiz_add_func(F, "expr", aux_expr, NULL);
-	fiz_add_func(F, "eq", aux_eqne, NULL);
-	fiz_add_func(F, "ne", aux_eqne, NULL);
-	fiz_add_func(F, "incr", aux_incr, NULL);
-	fiz_add_func(F, "decr", aux_incr, NULL);
-	fiz_add_func(F, "dict", aux_dict, NULL);
-	fiz_add_func(F, "include", aux_include, NULL);
+void fiz_add_aux(struct Fiz *F) {
+    fiz_add_func(F, "puts", aux_puts, NULL);
+    fiz_add_func(F, "expr", aux_expr, NULL);
+    fiz_add_func(F, "eq", aux_eqne, NULL);
+    fiz_add_func(F, "ne", aux_eqne, NULL);
+    fiz_add_func(F, "incr", aux_incr, NULL);
+    fiz_add_func(F, "decr", aux_incr, NULL);
+    fiz_add_func(F, "dict", aux_dict, NULL);
+    fiz_add_func(F, "include", aux_include, NULL);
 }
--- expr.c
+++ expr.c
@@ -1,9 +1,9 @@
 /*
  * A simple recursive descent expression evaluator.
- * 
+ *
  * Compile the test program like so:
  * $ gcc -o expr -Wall -Werror -pedantic -O3 -DTEST expr.c
- * 
+ *
  * This is free and unencumbered software released into the public domain.
  * http://unlicense.org/
  */
@@ -12,40 +12,36 @@
 #include <setjmp.h>
 #include <assert.h>
 
-struct expr_handler
-{
-	const char *str, *prev;
-	jmp_buf on_err;
-	const char **err;
+struct expr_handler {
+    const char *str, *prev;
+    jmp_buf on_err;
+    const char **err;
 };
 
-static void do_error(struct expr_handler *eh, const char *msg)
-{
-	*(eh->err) = msg;
-	longjmp(eh->on_err, 1);
+static void do_error(struct expr_handler *eh, const char *msg) {
+    *(eh->err) = msg;
+    longjmp(eh->on_err, 1);
 }
 
-static char getnext(struct expr_handler *h)
-{
-	char c;
-	while(isspace(h->str[0])) h->str++;
-	h->prev = h->str;
-	c = h->str[0];
-	h->str++;
-	return c;
+static char getnext(struct expr_handler *h) {
+    char c;
+    while(isspace(h->str[0])) h->str++;
+    h->prev = h->str;
+    c = h->str[0];
+    h->str++;
+    return c;
 }
 
 /* Macros that hopefully makes my intention clearer */
 #define peeknext(h) (h->str[0])
 #define gobblenext(h) (h->str++)
 
-static void reset(struct expr_handler *h)
-{
-	/* We're only looking ahead one token, so don't
-	 * use it in situations where this may happen: */
-	assert(h->prev != NULL);
-	h->str = h->prev;
-	h->prev = NULL;
+static void reset(struct expr_handler *h) {
+    /* We're only looking ahead one token, so don't
+     * use it in situations where this may happen: */
+    assert(h->prev != NULL);
+    h->str = h->prev;
+    h->prev = NULL;
 }
 
 /* Prototypes for the recursive descent parser's functions defined below */
@@ -60,183 +56,156 @@ static int atom(struct expr_handler *h);
 
 /* Entry point for the expression evaluator.
  * - 'str' contains the expression to be parsed.
- * - 'err' should be a pointer to a (const char *). When the 
+ * - 'err' should be a pointer to a (const char *). When the
  *   function returns, if the pointer pointed to by 'err' is NULL
  *   the expression was parsed successfully, otherwise *err will
  *   point to a string containing an error message.
  */
-int expr(const char *str, const char **err)
-{	
-	int n;
-	const char *dummy;
-	struct expr_handler eh;	
-	if(err)
-	{
-		*err = NULL;
-		eh.err = err;
-	}
-	else
-		eh.err = &dummy;
-	eh.str = str;
-	eh.prev = NULL;
-		
-	if(setjmp(eh.on_err))
-		return 0;
-	n = or(&eh);
-	if(getnext(&eh) != '\0')
-		do_error(&eh, "end of expression expected");		
-	return n;
+int expr(const char *str, const char **err) {
+    int n;
+    const char *dummy;
+    struct expr_handler eh;
+    if(err) {
+        *err = NULL;
+        eh.err = err;
+    } else
+        eh.err = &dummy;
+    eh.str = str;
+    eh.prev = NULL;
+
+    if(setjmp(eh.on_err))
+        return 0;
+    n = or(&eh);
+    if(getnext(&eh) != '\0')
+        do_error(&eh, "end of expression expected");
+    return n;
 }
 
 /*
  * Recursive descent starts here.
  */
-static int or(struct expr_handler *h)
-{
-	int n = and(h), r;
-	while(getnext(h) == '|' && peeknext(h) == '|')
-	{
-		gobblenext(h);
-		/* Short-circuit caught me here:
-		 * If you try to write this as n = n || and(h);
-		 * and n is "true", then the and(h) won't get called,
-		 * meaning the pointer h->str won't be moved along, causing
-		 * problems down the road.
-		 * I just hope a smart optimiser doesn't cause problems
-		 * (so far I've checked GCC, and it doesn't), so test with 
-		 * something like "1||0" or "0&&1" to check for problems
-		 */
-		r = and(h); 
-		n = n || r;
-	}
-	reset(h);
-	return n;
+static int or(struct expr_handler *h) {
+    int n = and(h), r;
+    while(getnext(h) == '|' && peeknext(h) == '|') {
+        gobblenext(h);
+        /* Short-circuit caught me here:
+         * If you try to write this as n = n || and(h);
+         * and n is "true", then the and(h) won't get called,
+         * meaning the pointer h->str won't be moved along, causing
+         * problems down the road.
+         * I just hope a smart optimiser doesn't cause problems
+         * (so far I've checked GCC, and it doesn't), so test with
+         * something like "1||0" or "0&&1" to check for problems
+         */
+        r = and(h);
+        n = n || r;
+    }
+    reset(h);
+    return n;
 }
 
-static int and(struct expr_handler *h)
-{
-	int n = not(h), r;
-	while(getnext(h) == '&' && peeknext(h) == '&')
-	{
-		gobblenext(h);
-		/* potential short-circuit problem; see or() above */
-		r = not(h); 
-		n = n && r;
-	}
-	reset(h);
-	return n;
+static int and(struct expr_handler *h) {
+    int n = not(h), r;
+    while(getnext(h) == '&' && peeknext(h) == '&') {
+        gobblenext(h);
+        /* potential short-circuit problem; see or() above */
+        r = not(h);
+        n = n && r;
+    }
+    reset(h);
+    return n;
 }
 
-static int not(struct expr_handler *h)
-{	
-	if(getnext(h) == '!')
-		return !comp(h);
-	reset(h);
-	return comp(h);
+static int not(struct expr_handler *h) {
+    if(getnext(h) == '!')
+        return !comp(h);
+    reset(h);
+    return comp(h);
 }
 
-static int comp(struct expr_handler *h)
-{
-	int n = term(h);
-	char c = getnext(h);
-	if(c == '=' || c == '>' || c == '<'
-		|| (c == '!' && peeknext(h) == '='))
-	{
-		if(h->str[0] == '=')
-		{
-			gobblenext(h);
-			switch(c)
-			{
-				case '=' : return n == term(h);
-				case '>' : return n >= term(h);
-				case '<' : return n <= term(h);
-				case '!' : return n != term(h);
-			}
-		}
-		else
-		{
-			switch(c)
-			{
-				case '=' : return n == term(h);
-				case '>' : return n > term(h);
-				case '<' : return n < term(h);
-			}
-		}
-	}
-	else
-		reset(h);
-	return n;
+static int comp(struct expr_handler *h) {
+    int n = term(h);
+    char c = getnext(h);
+    if(c == '=' || c == '>' || c == '<' || (c == '!' && peeknext(h) == '=')) {
+        if(h->str[0] == '=') {
+            gobblenext(h);
+            switch(c) {
+                case '=' : return n == term(h);
+                case '>' : return n >= term(h);
+                case '<' : return n <= term(h);
+                case '!' : return n != term(h);
+            }
+        } else {
+            switch(c) {
+                case '=' : return n == term(h);
+                case '>' : return n > term(h);
+                case '<' : return n < term(h);
+            }
+        }
+    } else
+        reset(h);
+    return n;
 }
 
-static int term(struct expr_handler *h)
-{
-	int n = factor(h), c;
-	while((c=getnext(h)) == '+' || c == '-')
-	{
-		if(c == '+')
-			n += factor(h);
-		else
-			n -= factor(h);
-	}
-	reset(h);
-	return n;
+static int term(struct expr_handler *h) {
+    int n = factor(h), c;
+    while((c=getnext(h)) == '+' || c == '-') {
+        if(c == '+')
+            n += factor(h);
+        else
+            n -= factor(h);
+    }
+    reset(h);
+    return n;
 }
 
-static int factor(struct expr_handler *h)
-{
-	int n = unary(h), c;
-	while((c=getnext(h)) == '*' || c == '/' || c == '%')
-	{
-		int x = unary(h);		
-		if(c == '*')
-			n *= x;
-		else 
-		{
-			if(x == 0) do_error(h, "divide by zero");
-			if(c == '/')
-				n /= x;
-			else
-				n %= x;
-		}
-	}
-	reset(h);
-	return n;
+static int factor(struct expr_handler *h) {
+    int n = unary(h), c;
+    while((c=getnext(h)) == '*' || c == '/' || c == '%') {
+        int x = unary(h);
+        if(c == '*')
+            n *= x;
+        else {
+            if(x == 0) do_error(h, "divide by zero");
+            if(c == '/')
+                n /= x;
+            else
+                n %= x;
+        }
+    }
+    reset(h);
+    return n;
 }
 
-static int unary(struct expr_handler *h)
-{	
-	int c = getnext(h);
-	if(c == '-' || c == '+')
-	{
-		if(c == '-')
-			return -atom(h);
-		return atom(h);
-	}
-	reset(h);
-	return atom(h);
+static int unary(struct expr_handler *h) {
+    int c = getnext(h);
+    if(c == '-' || c == '+') {
+        if(c == '-')
+            return -atom(h);
+        return atom(h);
+    }
+    reset(h);
+    return atom(h);
 }
 
-static int atom(struct expr_handler *h)
-{
-	int n = 0;
-	
-	if(getnext(h) == '(')
-	{
-		n = or(h);
-		if(getnext(h) != ')')
-			do_error(h, "missing ')'");
-		return n;
-	}
-	reset(h);
-	while(isspace(h->str[0])) h->str++;
-	if(!isdigit(h->str[0]))
-		do_error(h, "number expected");
-	while(isdigit(h->str[0]))
-	{
-		n = n * 10 + (h->str[0] - '0');
-		h->str++;
-	}	
-	return n;
+static int atom(struct expr_handler *h) {
+    int n = 0;
+
+    if(getnext(h) == '(') {
+        n = or(h);
+        if(getnext(h) != ')')
+            do_error(h, "missing ')'");
+        return n;
+    }
+    reset(h);
+    while(isspace(h->str[0])) h->str++;
+    if(!isdigit(h->str[0]))
+        do_error(h, "number expected");
+    while(isdigit(h->str[0])) {
+        n = n * 10 + (h->str[0] - '0');
+        h->str++;
+    }
+    return n;
 }
 
 /**********************************************************************
@@ -244,19 +213,17 @@ static int atom(struct expr_handler *h)
  **********************************************************************/
 #ifdef TEST
 #include <stdio.h>
-int main(int argc, char *argv[])
-{
-	int i, r;
-	const char *err;
-	for(i = 1; i < argc; i++)
-	{
-		r = expr(argv[i], &err);
-		if(!err)
-			printf("%s = %d\n", argv[i], r);
-		else
-			fprintf(stderr, "error: %s: %s\n", argv[i], err);
-	}
-	return 0;
+int main(int argc, char *argv[]) {
+    int i, r;
+    const char *err;
+    for(i = 1; i < argc; i++) {
+        r = expr(argv[i], &err);
+        if(!err)
+            printf("%s = %d\n", argv[i], r);
+        else
+            fprintf(stderr, "error: %s: %s\n", argv[i], err);
+    }
+    return 0;
 }
 
 #endif /* TEST */
